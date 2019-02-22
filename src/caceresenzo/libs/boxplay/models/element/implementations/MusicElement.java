8aed968b9fdd6e6f45838025db83dc0158c8dc7e
Caceresenzo <caceresenzo1502@gmail.com>
2019-02-22 17:55:18 +0100

    The Big Clean Up!

--- src/caceresenzo/libs/boxplay/models/element/implementations/MusicElement.java
+++
@@ -1,9 +0,0 @@
-package caceresenzo.libs.boxplay.models.element.implementations;
-
-public class MusicElement extends StoreElement {
-	
-	protected MusicElement(String identifier) {
-		super(identifier);
-	}
-	
-}
