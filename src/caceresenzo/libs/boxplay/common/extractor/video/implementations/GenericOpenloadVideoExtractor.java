6d5955f7f555958fc609d00561f990a03a43ce13
Caceresenzo <caceresenzo1502@gmail.com>
2019-03-03 15:33:32 +0100

    Implemented new Openload system (not that much change)

--- src/caceresenzo/libs/boxplay/common/extractor/video/implementations/GenericOpenloadVideoExtractor.java
+++ src/caceresenzo/libs/boxplay/common/extractor/video/implementations/GenericOpenloadVideoExtractor.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 
+import caceresenzo.libs.aa.AADecoder;
 import caceresenzo.libs.boxplay.common.extractor.video.VideoContentExtractor;
 import caceresenzo.libs.boxplay.culture.searchngo.providers.ProviderHelper;
 import caceresenzo.libs.boxplay.utils.Sandbox;
 import caceresenzo.libs.http.client.webb.Webb;
-import caceresenzo.libs.http.client.webb.WebbConstante;
 import caceresenzo.libs.string.StringUtils;
 
 /**
@@ -20,11 +20,6 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 	
 	/* Constants */
 	public static final String FILE_DELETED = "We can't find the file you are looking for. It maybe got deleted by the owner or was removed due a copyright violation.";
-	
-	/**
-	 * This regex is used to extract the "japanease smiley" encoded javascript at the end of the page that is used to decypher a key to watch the video
-	 */
-	public static final String REGEX_DOM_DATA_EXTRACTOR = "\\<div\\sclass=\\\"\\\"\\sstyle=\\\"display:none;\\\"\\>[ \\t\\n]*\\<p\\sstyle=\\\"\\\"\\sid=\\\"(.*?)\\\"\\>(.*?)\\<\\/p\\>[ \\t\\n]*\\<p\\sstyle=\\\"\\\"\\sclass=\\\"\\\"\\sid=\\\"DtsBlkVFQx\\\"\\>(.*?)\\<\\/p\\>[ \\t\\n]*\\<\\/div\\>";
 	
 	@Override
 	public String extractDirectVideoUrl(String url, VideoContentExtractorProgressCallback progressCallback) {
@@ -49,16 +44,16 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 		
 		getLogger().appendln("Parameters: ").appendln(" - BASE: " + baseUrlFormat).appendln();
 		
-		String openloadHtml;
+		String html;
 		try {
 			getLogger().appendln("Downloading target page: " + url);
-			openloadHtml = Webb.create().get(url).header(WebbConstante.HDR_USER_AGENT, WebbConstante.DEFAULT_USER_AGENT).ensureSuccess().asString().getBody();
+			html = Webb.create().get(url).chromeUserAgent().ensureSuccess().asString().getBody();
 			
-			if (!StringUtils.validate(openloadHtml)) {
+			if (!StringUtils.validate(html)) {
 				throw new NullPointerException("Download string is null.");
 			}
 			
-			getLogger().appendln("-- Finished > size=" + openloadHtml.length());
+			getLogger().appendln("-- Finished > size=" + html.length());
 		} catch (Exception exception) {
 			failed(true).notifyException(exception);
 			getLogger().appendln("-- Finished > failed=" + exception.getLocalizedMessage());
@@ -67,7 +62,7 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 		
 		getLogger().separator();
 		
-		if (!checkStreamingAvailability(openloadHtml)) {
+		if (!checkStreamingAvailability(html)) {
 			if (progressCallback != null) {
 				progressCallback.onFileNotAvailable();
 			}
@@ -76,7 +71,7 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 			
 			return null;
 		} else {
-			String extractedKey = new OpenloadUrlDecoderSandbox().execute(openloadHtml);
+			String extractedKey = new OpenloadVideoIdComputeSandbox().execute(html);
 			
 			getLogger().appendln("Computed key: " + extractedKey);
 			
@@ -93,11 +88,11 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 	}
 	
 	/**
-	 * Used to check if the target file to stream is available or not
+	 * Used to check if the target file to stream is available or not.
 	 * 
 	 * @param html
-	 *            Source of the page
-	 * @return Yes or not
+	 *            Source of the page.
+	 * @return Weather or not the {@link #FILE_DELETED} message is not on the page.
 	 */
 	public boolean checkStreamingAvailability(String html) {
 		if (html == null) {
@@ -115,281 +110,281 @@ public class GenericOpenloadVideoExtractor extends VideoContentExtractor {
 	@Override
 	public boolean matchUrl(String baseUrl) {
 		/* Custom from provider: full-stream/, belike1.stream/ */
-		return baseUrl.matches(".*?(oload\\.stream|oload\\.download|oload\\.fun|openload\\.co|oloadcdn\\.net|full-stream\\.co\\/|belike1\\.stream\\/).*?");
+		return baseUrl.matches(".*?(oload\\..+|openload\\.co|oloadcdn\\.net|full-stream\\.co\\/|belike1\\.stream\\/).*?");
 	}
 	
 	/**
-	 * This class is supposed to be able to calculate the link for a Openload Video in java. This will much faster than the old method.<br>
+	 * This class is supposed to be able to compute the id of an openload video that will be client sensitive (decoded for an ip) in java. This will much faster than the old method.<br>
 	 * Kept in mind that this is really sensitive to changes, and can break at any time.<br>
-	 * 
+	 * <br>
 	 * Please put it in a try catch block, this function will throw plenty of error if anything goes not as espected.
 	 * 
+	 * @version 2
 	 * @author Enzo CACERES
 	 */
 	@SuppressWarnings("all")
-	public static class OpenloadUrlDecoderSandbox implements Sandbox<String, String> {
+	public static class OpenloadVideoIdComputeSandbox implements Sandbox<String, String> {
 		
 		/* Variables */
 		private ProviderHelper helper;
 		
 		/* Constructor */
-		public OpenloadUrlDecoderSandbox() {
+		public OpenloadVideoIdComputeSandbox() {
 			this.helper = ProviderHelper.getStaticHelper();
 		}
 		
 		/**
-		 * This function is an implementation of the obsfrucated javascript available on a openload video page.
+		 * This function is an implementation of the obsfrucated javascript code available on a openload video page.
 		 * 
-		 * @param source
-		 *            Html or js container present on the page that will be used to extract precious information
+		 * @param html
+		 *            Downloaded page's HTML of the video you want to compute the key from.
+		 * @throws IllegalStateException
+		 *             If the <code>html</code> parameter is not valid (<code>null</code> or empty).
+		 * @throws IllegalStateException
+		 *             If there are more than two aa-encoded javascript in the page.
+		 * @throws IllegalStateException
+		 *             If the source key failed to be found.
+		 * @throws IllegalStateException
+		 *             If at least one of the two value changer didn't find its new values.
 		 */
 		@Override
-		public String execute(String source) {
-			String extractedSourceKey = extractSourceKey(source);
-			long swifterKey1 = getKeySwifterValue(1, source);
-			long swifterKey2 = getKeySwifterValue(2, source);
+		public String execute(String html) {
+			if (!StringUtils.validate(html)) {
+				throw new IllegalStateException("HTML is not a valid String.");
+			}
 			
-			String finalKey = "";
+			List<String> aaEncodedScripts = AADecoder.extractAllAAEncodedStrings(html);
 			
-			String editableKey = extractedSourceKey;
+			if (aaEncodedScripts.size() != 2) {
+				throw new IllegalStateException("Invalid aa-encoded script count.");
+			}
 			
-			int chunckSize = 72;
-			Object chunck = editableKey.substring(0x0, chunckSize);
+			String sourceKey;
+			try {
+				String decodedScript0 = AADecoder.decode(aaEncodedScripts.get(0));
+				String htmlDivId = helper.extract("\\+'(.*?)';", decodedScript0);
+				
+				sourceKey = helper.extract(String.format("<p id=\".*?\" style=\"\">(.*?)<\\/p>", htmlDivId), html);
+			} catch (Exception exception) {
+				throw new IllegalStateException("Failed to extract source key.", exception);
+			}
 			
+			Matcher valueChanger1 = helper.regex("_0x30725e=.*?parseInt\\('(\\d+?)'.*?\\)-(\\d+).*?-(\\d+?).*?\\((\\d+?)-", html);
+			Matcher valueChanger2 = helper.regex("_1x4bfb36=parseInt\\('(\\d+?)',8\\)-(\\d+?);", html);
+			
+			if (!(valueChanger1.find() && valueChanger2.find())) {
+				throw new IllegalStateException("One of the value changer hasn't found its values.");
+			}
+			
+			// 11
+			String _0x531f91 = sourceKey;
+			
+			// 12
+			// var _0x5d72cd = _0x531f91.charAt(0x0);
+			
+			// 13
+			String _0x5d72cd = _0x531f91;
+			
+			// 0
+			String _0x1bf6e5 = "";
+			
+			// 14
+			int _0x41e0ff = (0x9 * 0x8);
+			
+			// 3
+			int _0xccbe62 = _0x5d72cd.length();
+			
+			// 2
+			Object _0x439a49 = _0x5d72cd.substring(0x0, _0x41e0ff);
+			
+			// 9
 			List<Long> _0x3d7b02 = new ArrayList<>();
 			
-			Object _0x31f4aa = new KKeyPartHolder((String) chunck);
+			// 16
+			Object _0x31f4aa = new KKeHolder((String) _0x439a49);
 			
-			for (int i = 0; i < ((String) chunck).length(); i += 8) {
-				chunckSize = i * 8;
-				String data = ((String) chunck).substring(i, i + 8);
-				long value = Long.parseLong(data, 16);
+			// 1
+			for (int i = 0x0; i < ((String) _0x439a49).length(); i += 0x8) {
+				_0x41e0ff = i * 0x8;
 				
-				KKeyPartHolder.class.cast(_0x31f4aa).keyPart.add(value);
+				String _0x40b427 = ((String) _0x439a49).substring(i, i + 0x8);
+				long _0x577716 = Long.valueOf(_0x40b427, 0x10);
+				
+				{
+					if (false) {
+						_0x577716 = 0x0;
+					}
+					((KKeHolder) _0x31f4aa).ke.add(_0x577716);
+				}
 			}
 			
-			_0x3d7b02 = KKeyPartHolder.class.cast(_0x31f4aa).keyPart;
+			// 4
+			_0x3d7b02 = ((KKeHolder) _0x31f4aa).ke;
+			{
+				if (false) {
+					_0x3d7b02 = null;
+				}
+			}
 			
 			// 8
-			chunckSize = 72;
+			_0x41e0ff = 0x9 * 0x8;
 			
 			// 5
-			editableKey = editableKey.substring(chunckSize);
+			_0x5d72cd = _0x5d72cd.substring(_0x41e0ff);
 			
 			// 6
-			chunck = 0;
+			_0x439a49 = 0x0;
 			
 			// 15
-			long _0x145894 = 0;
+			int _0x145894 = 0x0;
 			
 			// 10
-			while ((int) chunck < editableKey.length()) {
-				// 5
-				long _0x5eb93a = 64;
+			while ((int) _0x439a49 < _0x5d72cd.length()) {
 				
-				// 8
-				long _0x37c346 = 127;
+				// 10 - 5
+				int _0x5eb93a = 0x40;
 				
-				// 0
-				long _0x896767 = 0;
+				// 10 - 8
+				int _0x37c346 = 0x7f;
 				
-				// 12
-				long _0x1a873b = 0;
+				// 10 - 0
+				long _0x896767 = 0x0;
 				
-				// 13
-				long _0x3d9c8e = 0;
+				// 10 - 12
+				int _0x1a873b = 0x0;
 				
-				// 9
-				_0x31f4aa = new MmXxHolder(128, 63);
+				// 10 - 13
+				long _0x3d9c8e = 0x0;
 				
-				// 10
+				// 10 - 9
+				_0x31f4aa = new MmXxHolder(0x80, 0x3f);
+				
+				// 10 - 10
 				do {
-					// 4
-					if ((int) chunck + 1 >= editableKey.length()) {
-						_0x5eb93a = 143;
+					// 10 - 10 - 4
+					if (((int) _0x439a49 + 0x1) >= _0x5d72cd.length()) {
+						_0x5eb93a = 0x8f;
 					}
 					
-					// 3
-					String _0x1fa71e = editableKey.substring((int) chunck, (int) chunck + 2);
+					// 10 - 10 - 3
+					String _0x1fa71e = _0x5d72cd.substring((int) _0x439a49, ((int) _0x439a49 + 0x2));
 					
-					// 0
-					chunck = ((int) chunck) + 1;
+					// 10 - 10 - 0
+					_0x439a49 = ((int) _0x439a49) + 1;
 					
-					// 5
-					chunck = ((int) chunck) + 1;
+					// 10 - 10 - 5
+					_0x439a49 = ((int) _0x439a49) + 1;
 					
-					// 6
-					_0x3d9c8e = Integer.parseInt(_0x1fa71e, 16);
+					// 10 - 10 - 6
+					_0x3d9c8e = Long.valueOf(_0x1fa71e, 0x10);
 					
-					// 2
-					if (_0x1a873b < 30) {
-						long _0x332549 = _0x3d9c8e & MmXxHolder.class.cast(_0x31f4aa).xx;
-						_0x896767 += _0x332549 << _0x1a873b;
-					} else {
-						long _0x332549 = _0x3d9c8e & MmXxHolder.class.cast(_0x31f4aa).xx;
-						_0x896767 += _0x332549 * Math.pow(2, _0x1a873b);
+					// 10 - 10 - 2
+					{
+						if (false) {
+							_0x3d9c8e += 10;
+							((MmXxHolder) _0x31f4aa).xx = 0x11;
+						}
+						
+						if (_0x1a873b < (0x6 * 0x5)) {
+							long _0x332549 = _0x3d9c8e & ((MmXxHolder) _0x31f4aa).xx;
+							_0x896767 += _0x332549 << _0x1a873b;
+						} else {
+							long _0x332549 = _0x3d9c8e & ((MmXxHolder) _0x31f4aa).xx;
+							_0x896767 += _0x332549 * Math.pow(0x2, _0x1a873b);
+						}
 					}
 					
-					// 1
-					_0x1a873b += 6;
+					// 10 - 10 - 1
+					_0x1a873b += 0x6;
+					
 				} while (_0x3d9c8e >= _0x5eb93a);
 				
-				// 4
+				// 10 - 4
 				long _0x59ce16 = 0x28a28dec;
 				
-				// 11
-				long _1x4bfb36 = swifterKey2;
+				// 10 - 11
+				long _1x4bfb36 = Long.valueOf(valueChanger2.group(1), 8) - Integer.valueOf(valueChanger2.group(2));
 				
-				// 6
-				long _0x30725e = _0x896767 ^ _0x3d7b02.get((int) (_0x145894 % 9));
+				// 10 - 6
+				long _0x30725e = _0x896767 ^ _0x3d7b02.get((_0x145894 % 0x9));
 				
-				// 3
-				_0x30725e = (_0x30725e ^ swifterKey1) ^ _1x4bfb36;
+				// 10 - 3
+				_0x30725e = (_0x30725e ^ ((Long.valueOf(valueChanger1.group(1), 8) - Integer.valueOf(valueChanger1.group(2)) + 0x4 - Integer.valueOf(valueChanger1.group(3))) / (Integer.valueOf(valueChanger1.group(4)) - 0x8))) ^ _1x4bfb36;
 				
-				// 1
-				long _0x2de433 = (_0x5eb93a * 2) + _0x37c346;
+				// 10 - 1
+				int _0x2de433 = (_0x5eb93a * 0x2) + _0x37c346;
 				
-				// 7
-				for (int i = 0; i < 4; i++) {
-					// 2
+				// 10 - 7
+				for (int i = 0x0; (i < 0x4); i++) {
+					// 10 - 7 - 2
 					long _0x1a9381 = _0x30725e & _0x2de433;
 					
-					// 0
-					long _0x1a0e90 = (chunckSize / 9) * i;
+					// 10 - 7 - 0
+					int _0x1a0e90 = (_0x41e0ff / 0x9) * i;
 					
-					// 5
+					// 10 - 7 - 5
 					_0x1a9381 = _0x1a9381 >> _0x1a0e90;
 					
-					// 4
-					char _0x3fa834 = (char) (_0x1a9381 - 1);
+					// 10 - 7 - 4
+					String _0x3fa834 = String.valueOf((char) (_0x1a9381 - 1));
 					
-					// 3
-					if (_0x3fa834 != '$') {
-						finalKey += _0x3fa834;
+					// 10 - 7 - 3
+					if (_0x3fa834.charAt(0) != '$') {
+						_0x1bf6e5 += _0x3fa834;
 					}
 					
-					// 1
-					_0x2de433 = _0x2de433 << (chunckSize / 9);
+					// 10 - 7 - 1
+					_0x2de433 = _0x2de433 << (_0x41e0ff / 0x9);
 				}
 				
-				// 2
-				_0x145894 += 1;
+				// 10 - 2
+				_0x145894 += 0x1;
+				
 			}
 			
-			return finalKey;
+			// 7
+			// $("#lqEH1").text(_0x1bf6e5);
+			
+			return _0x1bf6e5;
 		}
 		
 		/**
-		 * Extract encoded data that is needed to compute the final url.
+		 * Simple data holder.
 		 * 
-		 * @param source
-		 *            Html/js container source of the page to extract these values from
-		 * @return Extracted source key to work with
-		 * @throws IllegalArgumentException
-		 *             If the source parameter is null
-		 * @throws NullPointerException
-		 *             If source key not found, or if the code really do a {@link NullPointerException}
+		 * @author Enzo CACERES
 		 */
-		public String extractSourceKey(String source) {
-			if (!StringUtils.validate(source)) {
-				throw new IllegalArgumentException("Source can't be null.");
-			}
+		private static class KKeHolder {
 			
-			Matcher matcher = helper.regex("\\<div\\sclass\\=\\\"\\\"\\sstyle\\=\\\"display\\:none\\;\\\"\\>[\\s]*\\<p\\sstyle\\=\\\"\\\"\\sid\\=\\\".*?\\\"\\>(.*?)\\<\\/p\\>.*?\\<\\/div\\>", source);
-			
-			if (!matcher.find()) {
-				throw new NullPointerException("Source key not found.");
-			}
-			
-			return matcher.group(1).trim();
-		}
-		
-		/**
-		 * "key swifter" are little values that are changing depending of the video file.<br>
-		 * For an extraction to work, you will need to tweak these values in your code.<br>
-		 * <br>
-		 * Actually, there are 2 values that you need to change:
-		 * <ul>
-		 * <li>First occure at (relative) line 170 in the openload source page, here a regex to get this value: <a href="https://regex101.com/r/JZqOnj/1">regex101</a></li>
-		 * <li>The second occure at (relative) line 265, regex: <a href="https://regex101.com/r/UzbSWO/1">regex101</a></li>
-		 * </ul>
-		 * If one of the keys are not found, you must considere that the extraction is not possible.<br>
-		 * In that case, try to redownload the page, or tweak the regex.
-		 * 
-		 * @param id
-		 *            Id of the "swifter key" that you want, you can only put <code>1</code> or <code>2</code>
-		 * @param source
-		 *            Html/js container source of the page to extract these values from
-		 * @return Calculated values
-		 * @throws IllegalArgumentException
-		 *             If the source parameter is null
-		 * @throws IllegalStateException
-		 *             If no swifter key is available with the id you enter
-		 * @throws NullPointerException
-		 *             If swifter keys are not founds, or if the code really do a {@link NullPointerException}
-		 */
-		public long getKeySwifterValue(int id, String source) {
-			if (!StringUtils.validate(source)) {
-				throw new IllegalArgumentException("Source can't be null.");
-			}
-			
-			switch (id) {
-				case 1: {
-					Matcher matcher = helper.regex("\\(parseInt\\(\\'([\\d]*)\\'\\,8\\)\\-([\\d]*)\\+0x4\\-([\\d]*)\\)\\/\\(([\\d]*)-0x8\\)\\)", source);
-					
-					if (!matcher.find()) {
-						throw new NullPointerException("Key Swifter 1 not found.");
-					}
-					
-					long a = Long.parseLong(matcher.group(1), 8);
-					long b = Integer.parseInt(matcher.group(2));
-					long c = Integer.parseInt(matcher.group(3));
-					long d = Integer.parseInt(matcher.group(4));
-					
-					return ((a - b + 0x4 - c) / (d - 0x8));
-				}
-				
-				case 2: {
-					Matcher matcher = helper.regex("\\=parseInt\\('([\\d]*)'\\,8\\)-(.*?);", source);
-					
-					if (!matcher.find()) {
-						throw new NullPointerException("Key Swifter 2 not found.");
-					}
-					
-					long a = Long.parseLong(matcher.group(1), 8);
-					long b = Integer.parseInt(matcher.group(2));
-					
-					return (a - b);
-				}
-				
-				default: {
-					throw new IllegalStateException("No key swifter with id: " + id);
-				}
-			}
-		}
-		
-		private class KKeyPartHolder {
-			
+			/* Variables */
 			public String k;
-			public List<Long> keyPart;
+			public List<Long> ke;
 			
-			public KKeyPartHolder(String chunk) {
-				k = chunk;
-				keyPart = new ArrayList<>();
+			/* Constructor */
+			public KKeHolder(String k) {
+				this.k = k;
+				this.ke = new ArrayList<>();
 			}
 			
 		}
 		
-		private class MmXxHolder {
+		/**
+		 * Simple data holder.
+		 * 
+		 * @author Enzo CACERES
+		 */
+		private static class MmXxHolder {
 			
+			/* Variables */
 			public int mm, xx;
 			
+			/* Constructor */
 			public MmXxHolder(int mm, int xx) {
 				this.mm = mm;
 				this.xx = xx;
 			}
 			
 		}
+		
 	}
 	
 }
